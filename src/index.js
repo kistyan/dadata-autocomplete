1c1f409d5c29c39887c182b6a2dc97934cafe9e7
kistyan <kistyan12@GITHUB.COM>
2023-07-13 11:45:17 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import axios from 'axios';
+
+const DADATA_TOKEN = 'b64560af54b95583ac2711f8ecb9744514f08b11';
+
+export default function DaDataAutocomplete({
+    type='address',
+    label='Адрес',
+    initialQuery='',
+    onChange=(event, newValue) => {console.log(newValue);},
+    inputProps
+}) {
+    const [suggestions, setSuggestions] = useState([]);
+    const [inputValue, setInputValue] = useState(initialQuery);
+
+    const updateSuggestions = (newInputValue) => {
+        if (newInputValue) {
+            axios.post(
+                'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/' + type,
+                {
+                    query: newInputValue
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'Authorization': 'Token ' + DADATA_TOKEN
+                    }
+                }
+            ).then(
+                (response) => {
+                    setSuggestions(response.data.suggestions);
+                }
+            ).catch(
+                (error) => {
+                    console.log(error);
+                }
+            );
+        }
+        else {
+            setSuggestions([]);
+        }
+    }
+
+    const handleFocus = (event, newInputValue) => {
+        updateSuggestions(event.target.value);
+    }
+
+    const handleInputChange = (event, newInputValue) => {
+        setInputValue(newInputValue);
+        updateSuggestions(newInputValue);
+    }
+
+    const handleChange = (event, newValue) => {
+        onChange(event, newValue);
+    }
+
+    return (
+        <Autocomplete
+            freeSolo={true}
+            filterOptions={(x) => x}
+            options={suggestions}
+            getOptionLabel={(option) => option.value}
+            onInputChange={handleInputChange}
+            onFocus={handleFocus}
+            onChange={handleChange}
+            inputValue={inputValue}
+            {...inputProps}
+            renderInput={(params) => <TextField {...params} label={label} />}
+            renderOption={(props, option, {inputValue}) => {
+                return (
+                    <li {...props}>
+                        <div>
+                            {option.value}
+                            {type === 'fms_unit' && (
+                                <span style={{fontSize: '0.8rem'}}>
+                                    <br />
+                                    Код подразделения {option.data.code}
+                                </span>
+                            )}
+                        </div>
+                    </li>
+                );
+            }}
+        />
+    );
+}
